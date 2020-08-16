a6f73b390cbc29c4bd363682f50c0a0a744085e8
Gilbert <ldes89150@gmail.com>
2020-08-15 23:39:36 -0400

    try circleci...

--- test/test_backend.py
+++ test/test_backend.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.mark.skip(reason="This test has to be separated from the other test.")
+#@pytest.mark.skip(reason="This test has to be separated from the other test.")
 def test_backend():
     import pySLM2
     assert pySLM2.BACKEND._initialized is False
