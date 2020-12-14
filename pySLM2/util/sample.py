6fea2def6e4a6d25f32b922c252f674cdaab85c2
Gilbert <ldes89150@gmail.com>
2020-12-14 15:51:55 -0500

    fix path

--- pySLM2/util/sample.py
+++ pySLM2/util/sample.py
@@ -4,7 +4,7 @@ import numpy as np
 
 __all__ = ["number_image"]
 
-_font_path = os.path.join(__file__, "NotoMono-Regular.ttf")
+_font_path = os.path.join(os.path.dirname(__file__), "NotoMono-Regular.ttf")
 
 
 def number_image(i, Nx, Ny):
@@ -13,3 +13,4 @@ def number_image(i, Nx, Ny):
     fnt_default = ImageFont.truetype(_font_path, size=Ny // 2)
     draw.text((0, Ny // 4), str(i), (1), font=fnt_default, align='center')
     return np.array(img, dtype=np.bool)
+
