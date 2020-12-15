0dd5a06188c4386e8d0dd2adc6d60357d8265616
Gilbert <ldes89150@gmail.com>
2020-12-15 18:36:14 -0500

    update doc

--- pySLM2/util/sample.py
+++ pySLM2/util/sample.py
@@ -8,6 +8,22 @@ _font_path = os.path.join(os.path.dirname(__file__), "NotoMono-Regular.ttf")
 
 
 def number_image(i, Nx, Ny):
+    """Create a image that shows the number.
+
+    Parameters
+    ----------
+    i: int
+        Number to be displayed on the DMD.
+    Nx: int
+        Number of pixel of the DMD in x direction (width).
+    Ny: int
+        Number of pixel of the DMD in y direction (height).
+    Returns
+    -------
+    img: numpy.ndarray
+        The binary image that contains the number.
+
+    """
     img = Image.new('1', (Nx, Ny))
     draw = ImageDraw.Draw(img)
     fnt_default = ImageFont.truetype(_font_path, size=Ny // 2)
