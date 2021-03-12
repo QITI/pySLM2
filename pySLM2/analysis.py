cf15a35f85de9fbd02ee8629578629cea9b7a430
Gilbert <ldes89150@gmail.com>
2021-03-12 16:09:22 -0500

    update docstring and test cases

--- pySLM2/analysis.py
+++ pySLM2/analysis.py
@@ -2,7 +2,27 @@ from .profile import Zernike
 import numpy as np
 
 
-def remove_trivial_phase(profile, radius, remove_defocus=False):
+def remove_trivial_phase(profile, radius, remove_defocus=False, verbose=False):
+    """Remove the trivial phase map from the profile within the aperture.
+
+    The trivial phase maps include piston and tilt by default.
+
+    Parameters
+    ----------
+    profile: numpy.ndarray
+        The phase map profile
+    radius: int or float
+        The aperture radiusa
+    remove_defocus: bool
+        If true, remove the defocus from the profile.
+    verbose: bool
+        If true, print the coefficient of the removed phase map.
+    Returns
+    -------
+    profile_nontrivial: numpy.ndarray
+        Phase map after removing trivial
+
+    """
     Ny, Nx = profile.shape
     xx, yy = np.meshgrid(np.arange(Nx) - Nx // 2, np.arange(Ny, 0, -1) - Ny // 2)
     trivial_terms = [(0, 0), (1, -1), (1, 1)]
@@ -15,8 +35,7 @@ def remove_trivial_phase(profile, radius, remove_defocus=False):
     for n, m in trivial_terms:
         z = Zernike(1, radius, n, m, extrapolate=False)(xx, yy)
         c = np.mean(profile[aperture] * z[aperture])
-        print(n,m,c)
-
+        if verbose:
+            print("n = {0}, m = {1}, coefficient = {2}".format(n, m, c))
         profile = profile - c * z
-    # profile = profile - np.mean(profile[r<radius])
     return profile
--- test/test_analysis.py
+++ test/test_analysis.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pytest
 import pySLM2
-import matplotlib.pyplot as plt
+
 
 def test_remove_trivial_phase_piston():
-    radius=500
+    radius = 500
     xx, yy = np.meshgrid(np.arange(1920) - 1920 // 2, np.arange(1080, 0, -1) - 1080 // 2)
 
     profile = pySLM2.Zernike(1,radius,0,0)(xx,yy)
@@ -13,16 +13,17 @@ def test_remove_trivial_phase_piston():
 
     profile = pySLM2.Zernike(1,radius,1,1)(xx,yy) + pySLM2.Zernike(1,radius,0,0)(xx,yy)
     profile_removed = pySLM2.remove_trivial_phase(profile, radius=radius)
-
-    plt.imshow(profile_removed)
-    plt.colorbar()
-    plt.show()
     np.testing.assert_array_almost_equal(profile_removed, np.zeros_like(profile_removed), decimal=3)
-    # Can we improve the precision
+    # Can we improve the precision?
 
-    #profile = np.array([np.linspace(-1, 1, 1920) for _ in range(1080)])
-    #profile_removed = pySLM2.remove_trivial_phase(profile, radius=500, extrapolate=True)
-    #np.testing.assert_array_almost_equal(profile_removed, np.zeros_like(profile_removed), decimal=4)
+    profile = pySLM2.Zernike(1, radius, 2, 0)(xx, yy)
+    profile_removed = pySLM2.remove_trivial_phase(profile, radius=radius)
+    np.testing.assert_array_almost_equal(profile_removed, profile, decimal=3)
+
+    profile = pySLM2.Zernike(1, radius, 2, 0)(xx, yy)
+    profile_removed = pySLM2.remove_trivial_phase(profile, radius=radius, remove_defocus=True)
+    np.testing.assert_array_almost_equal(profile_removed, np.zeros_like(profile_removed), decimal=3)
+
 
 
 
