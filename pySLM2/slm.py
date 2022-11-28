62a34f87cab839d957385ab4576f2f5e6d566c0f
Gilbert Shih <ldes89150@gmail.com>
2022-11-28 15:33:28 -0500

    update

--- pySLM2/slm.py
+++ pySLM2/slm.py
@@ -275,13 +275,14 @@ class DMD(SLM):
 
         phase_in = tf.math.angle(input_profile)
         amp_in = tf.math.abs(input_profile)
-        
+
+        amp_out = tf.math.abs(target_profile_fp)
+
         phase_out = tf.where(
             target_profile_fp == 0.0,
-            tf.zeros_like(target_profile_fp),
+            tf.zeros_like(amp_out),
             tf.math.angle(target_profile_fp)
         )
-        amp_out = tf.math.abs(target_profile_fp)
 
         amp_scaled = tf.where(
             amp_out == 0.0,
