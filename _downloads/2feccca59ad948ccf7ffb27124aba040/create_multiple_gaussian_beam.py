9d64481e20f57436093bd4b3426742493e608a98
ldes89150 <ldes89150@gmail.com>
2023-11-23 22:03:28 +0000

    Deploying to gh-pages from @ QITI/pySLM2@00a849cc82bd88a25740024e199eef349d8041e0 🚀

--- _downloads/2feccca59ad948ccf7ffb27124aba040/create_multiple_gaussian_beam.py
+++ _downloads/2feccca59ad948ccf7ffb27124aba040/create_multiple_gaussian_beam.py
@@ -2,6 +2,7 @@ import pySLM2
 from scipy.constants import micro, nano, milli
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 
 dmd = pySLM2.DLP9500(
     369 * nano,  # wavelength
@@ -27,9 +28,13 @@ pfig, axs = plt.subplots(2, 2)
 
 axs[0, 0].set_title("Hologram displayed on the DMD")
 axs[0, 0].imshow(dmd.dmd_state)
+axs[0, 0].set_xlabel("x [px]")
+axs[0, 0].set_ylabel("y [px]")
 
 axs[1, 0].set_title("Input beam profile")
 axs[1, 0].imshow(dmd.profile_to_tensor(input_profile ** 2))
+axs[1, 0].set_xlabel("x [px]")
+axs[1, 0].set_ylabel("y [px]")
 
 sim = pySLM2.DMDSimulation(dmd, padding_x=0, padding_y=(dmd.Nx-dmd.Ny)//2)
 
@@ -37,11 +42,17 @@ sim = pySLM2.DMDSimulation(dmd, padding_x=0, padding_y=(dmd.Nx-dmd.Ny)//2)
 sim.propagate_to_image(input_profile)
 sim.block_zeroth_order()
 
-axs[0, 1].pcolormesh(*sim.image_plane_padded_grid, sim.image_plane_intensity)
+def to_um(x, pos):
+    return f"{x/micro: .1f} $\mu$m"
+
+p = axs[0, 1].pcolormesh(*sim.image_plane_padded_grid, sim.image_plane_intensity)
 (x,y) = dmd.first_order_origin
 axs[0, 1].set_title("Intensity profile of the first order beam")
 axs[0, 1].set_xlim(x-100*micro, x+100*micro)
 axs[0, 1].set_ylim(y-100*micro, y+100*micro)
+axs[0, 1].xaxis.set_major_formatter(FuncFormatter(to_um))
+axs[0, 1].yaxis.set_major_formatter(FuncFormatter(to_um))
+plt.colorbar(p, ax=axs[0, 1])
 
 p = axs[1, 1].pcolormesh(*sim.image_plane_padded_grid, np.angle(sim.image_plane_field))
 (x,y) = dmd.first_order_origin
@@ -49,5 +60,7 @@ axs[1, 1].set_title("Phase profile of the first order beam")
 plt.colorbar(p, ax=axs[1, 1])
 axs[1, 1].set_xlim(x-100*micro, x+100*micro)
 axs[1, 1].set_ylim(y-100*micro, y+100*micro)
+axs[1, 1].xaxis.set_major_formatter(FuncFormatter(to_um))
+axs[1, 1].yaxis.set_major_formatter(FuncFormatter(to_um))
 plt.tight_layout()
 plt.show()
